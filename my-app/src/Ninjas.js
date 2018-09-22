d15cdcf63c19a69c39bdeaad8868301e7d343681
Your Name <you@example.com>
2018-09-22 22:10:57 +0530

    Conditional Rendering

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,19 +1,22 @@
 import React from 'react'
 const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-                <hr/>
-            </div>
-        );
+        if( ninja.age > 22) {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                    <hr/>
+                </div>
+            );
+        } else {
+            return null
+        }
     })
     return(
         <div className="ninja-list">
             {ninjaList}
-            
         </div>  
     );
 }
