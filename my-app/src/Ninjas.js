7048d73ecaaac1628b82741432cee6d4c8ca030f
Your Name <you@example.com>
2018-09-22 22:20:05 +0530

    Conditional rendering with ternary oprator

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,18 +1,28 @@
 import React from 'react'
 const Ninjas = ({ninjas}) => {
-    const ninjaList = ninjas.map(ninja => {
-        if( ninja.age > 22) {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                    <hr/>
-                </div>
-            );
-        } else {
-            return null
-        }
+    // const ninjaList = ninjas.map(ninja => {
+    //     if( ninja.age > 22) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: {ninja.name}</div>
+    //                 <div>Age: {ninja.age}</div>
+    //                 <div>Belt: {ninja.belt}</div>
+    //                 <hr/>
+    //             </div>
+    //         );
+    //     } else {
+    //         return null
+    //     }
+    // })
+    
+    const ninjaList = ninjas.map((ninja) => {
+        return (ninja.age > 22) ? (
+            <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>age {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+            </div>
+        ) : null;
     })
     return(
         <div className="ninja-list">
